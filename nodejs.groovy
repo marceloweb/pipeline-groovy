0d8354ac2be136789688d79a897573357aebd16b
Marcelo Oliveira <marcelo@marceloweb.info>
2020-02-04 20:40:16 -0300

    - initial

---
+++ nodejs.groovy
@@ -0,0 +1,18 @@
+job('nodejs-demo-groovy') {
+    scm {
+        git('git://github.com/marceloweb/nodejs-demo.git') {  node -> // is hudson.plugins.git.GitSCM
+            node / gitConfigName('DSL User')
+            node / gitConfigEmail('jenkins-dsl@newtech.academy')
+        }
+    }
+    triggers {
+        scm('H/5 * * * *')
+    }
+    wrappers {
+        nodejs('nodejs') // this is the name of the NodeJS installation in 
+                         // Manage Jenkins -> Configure Tools -> NodeJS Installations -> Name
+    }
+    steps {
+        shell("npm install")
+    }
+}
