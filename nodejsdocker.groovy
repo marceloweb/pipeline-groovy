a6015fc42bf69bf5986301d56eca2e9206f42177
Marcelo Oliveira <marcelo@marceloweb.info>
2020-02-04 23:58:35 -0300

    - adjustments: pipeline

--- nodejsdocker.groovy
+++ nodejsdocker.groovy
@@ -13,6 +13,14 @@ job('nodejs-demo-groovy') {
                          // Manage Jenkins -> Configure Tools -> NodeJS Installations -> Name
     }
     steps {
-        shell("npm install")    
+        dockerBuildAndPublish {
+            repositoryName('marceloweb/docker-nodejs-demo')
+            tag('${GIT_REVISION,length=9}')
+            registryCredentials('dockerhub')
+            forcePull(false)
+            forceTag(false)
+            createFingerprints(false)
+            skipDecorate()
+        }
     }
 }
